9411da13aa3854fdbc9ce06ae6699047c9cdace1
David Dionisio <garapata@gmail.com>
2013-02-28 22:37:43 -0800

    bunch more art (level background 1), removed some crap

---
+++ Taurus/Assets/M8/Editor/2DToolkit/tk2dSetColorHierarchy.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.IO;
+
+namespace M8.Editor {
+    public class tk2dSetColorHierarchy : EditorWindow {
+
+        [MenuItem("M8/tk2d/Set Color Hierarchy")]
+        static void DoIt() {
+            EditorWindow.GetWindow(typeof(tk2dSetColorHierarchy));
+        }
+
+        private Color mColor = Color.white;
+        private bool mIncludeInactive = true;
+        private Vector2 mScrollPos = Vector2.zero;
+
+        void OnGUI() {
+            EditorGUIUtility.LookLikeControls(80.0f);
+
+            GUI.skin.label.wordWrap = true;
+
+            Object[] objs = Selection.objects;
+
+            mColor = EditorGUILayout.ColorField(mColor);
+
+            NGUIEditorTools.DrawSeparator();
+
+            mIncludeInactive = GUILayout.Toggle(mIncludeInactive, "Include Inactive");
+
+            if(GUILayout.Button("Apply")) {
+                foreach(GameObject go in objs) {
+                    if(go != null)
+                        ApplyColorHierarchy(go);
+                }
+
+                SceneView.RepaintAll();
+                
+            }
+
+            NGUIEditorTools.DrawSeparator();
+
+            GUILayout.Label("Selected Objects:");
+
+            mScrollPos = GUILayout.BeginScrollView(mScrollPos);
+
+            foreach(GameObject go in objs) {
+                if(go != null)
+                    GUILayout.Label(go.name);
+            }
+
+            GUILayout.EndScrollView();
+        }
+
+        void ApplyColorHierarchy(GameObject go) {
+            tk2dBaseSprite[] sprites = go.GetComponentsInChildren<tk2dBaseSprite>(mIncludeInactive);
+            foreach(tk2dBaseSprite spr in sprites) {
+                spr.color = mColor;
+            }
+        }
+    }
+}
